bc120af62efbe468d0bd9dd4f5aa3f361d3841b6
ilakshir <ilakshirohit31@gmail.com>
2023-10-29 16:45:30 -0700

    jh

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Webcam.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+import java.util.List;
+
+@TeleOp(name = "Concept: TensorFlow Object Detection Easy", group = "Concept")
+
+public class Webcam {
+
+    private static final boolean USE_WEBCAM = true;
+
+}
