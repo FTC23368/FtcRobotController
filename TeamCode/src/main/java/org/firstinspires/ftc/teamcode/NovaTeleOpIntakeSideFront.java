a4dafafdd5b855a9fd3419206a0aacd2a2b10398
maahumjahangir <maahumjahangir@gmail.com>
2023-11-04 15:52:49 -0700

    Debugged linear slides movement.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NovaTeleOpIntakeSideFront.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NovaTeleOpIntakeSideFront.java
@@ -103,16 +103,19 @@ public class NovaTeleOpIntakeSideFront extends LinearOpMode {
             // If dpad left is pressed, sliders up to medium height
                 if (gamepad2.dpad_left) {
                     pidMoveSliderToEncoderPosBrakeMode(1500, .4, 100);
+                    gamepad2.dpad_left = false;
                 }
 
                 // If dpad up is pressed, sliders up to high height
                 if (gamepad2.dpad_up) {
                     pidMoveSliderToEncoderPosBrakeMode(1800, .4, 100);
+                    gamepad2.dpad_up = false;
                 }
 
                 // If dpad down is pressed, sliders fully retract
                 if (gamepad2.dpad_down) {
                     pidMoveSliderToEncoderPosBrakeMode(0, .3, 100);
+                    gamepad2.dpad_down = false;
                 }
 
             // POCKET MOVEMENT --------------------------------------------------------------------|
