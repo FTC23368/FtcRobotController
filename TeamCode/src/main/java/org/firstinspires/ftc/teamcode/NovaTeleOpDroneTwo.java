aced20bbfba900cdd2cd7908a6b6cae7bfbc3892
ilakshir <ilakshirohit31@gmail.com>
2023-11-12 18:30:47 -0800

    new drone servo tester class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NovaTeleOpDroneTwo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NovaTeleOpDroneTwo.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class NovaTeleOpDroneTwo extends LinearOpMode{
@@ -10,14 +11,14 @@ public class NovaTeleOpDroneTwo extends LinearOpMode{
     public void runOpMode(){
 
         // declaring the servo
-        CRServo drone = hardwareMap.crservo.get("drone");
+        Servo drone = hardwareMap.servo.get("drone");
 
         waitForStart();
 
         // if 'a' pressed set power to full
         while(opModeIsActive()){
             if (gamepad1.a){
-                drone.setPower(1);
+                drone.setPosition(0.5);
             }
         }
     }
