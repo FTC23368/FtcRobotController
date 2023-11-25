f51f46830382ada700cb4b80ad15ca16b167dc2b
maahumjahangir <maahumjahangir@gmail.com>
2023-11-25 13:32:08 -0800

    Tested TFOD - IT WORKS!!!!!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NovaTensorFlowTeamPropDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NovaTensorFlowTeamPropDetection.java
@@ -61,7 +61,7 @@ public class NovaTensorFlowTeamPropDetection extends LinearOpMode {
     private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/myCustomModel.tflite";
     // Define the labels recognized in the model for TFOD (must be in training order!)
     private static final String[] LABELS = {
-       "Pixel",
+       "blue", "red", "none"
     };
 
     /**
@@ -122,17 +122,17 @@ public class NovaTensorFlowTeamPropDetection extends LinearOpMode {
             // will load the default model for the season. To define a custom model to load, 
             // choose one of the following:
             //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
-            //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
-            //.setModelAssetName(TFOD_MODEL_ASSET)
+            //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.\
+            .setModelAssetName(TFOD_MODEL_ASSET)
             //.setModelFileName(TFOD_MODEL_FILE)
 
             // The following default settings are available to un-comment and edit as needed to 
             // set parameters for custom models.
-            //.setModelLabels(LABELS)
-            //.setIsModelTensorFlow2(true)
-            //.setIsModelQuantized(true)
-            //.setModelInputSize(300)
-            //.setModelAspectRatio(16.0 / 9.0)
+            .setModelLabels(LABELS)
+            .setIsModelTensorFlow2(true)
+            .setIsModelQuantized(true)
+            .setModelInputSize(480)
+            .setModelAspectRatio(16.0 / 9.0)
 
             .build();
 
@@ -150,10 +150,10 @@ public class NovaTensorFlowTeamPropDetection extends LinearOpMode {
         //builder.setCameraResolution(new Size(640, 480));
 
         // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
-        //builder.enableLiveView(true);
+        builder.enableLiveView(true);
 
         // Set the stream format; MJPEG uses less bandwidth than default YUY2.
-        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+        builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
 
         // Choose whether or not LiveView stops if no processors are enabled.
         // If set "true", monitor shows solid orange screen if no processors enabled.
@@ -167,7 +167,7 @@ public class NovaTensorFlowTeamPropDetection extends LinearOpMode {
         visionPortal = builder.build();
 
         // Set confidence threshold for TFOD recognitions, at any time.
-        //tfod.setMinResultConfidence(0.75f);
+        tfod.setMinResultConfidence(0.75f);
 
         // Disable or re-enable the TFOD processor at any time.
         //visionPortal.setProcessorEnabled(tfod, true);
